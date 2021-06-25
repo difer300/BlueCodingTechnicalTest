4ca882cbcd59b250a2a8b1c3ed666d42b921feff
diegonavarroteam <diego.navarro@teaminternational.com>
2021-06-24 21:02:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BillsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
